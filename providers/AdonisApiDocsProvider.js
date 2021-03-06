78540e490e51232a5a48ba12f6b9bf832ca3e472
alepontes <alexandrelopespontes@gmail.com>
2021-03-06 12:15:49 -0300

    feat: serve template output in /docs

--- providers/AdonisApiDocsProvider.js
+++ providers/AdonisApiDocsProvider.js
@@ -1,18 +1,9 @@
 'use strict'
 
-const adonis = `../../../mandala-api/node_modules/@adonisjs`;
-
-// const { ServiceProvider } = require('@adonisjs/fold');
-const { ServiceProvider } = require(`${adonis}/fold`);
-
-// const { hooks } = require('@adonisjs/ignitor');
-const { hooks } = require(`${adonis}/ignitor`);
-
-// const RouteStore = require('@adonisjs/framework/src/Route/Store');
-const RouteStore = require(`${adonis}/framework/src/Route/Store`);
-
-// const Route = require('@adonisjs/framework/src/Route/index');
-const Route = require(`${adonis}/framework/src/Route/index`);
+const { ServiceProvider } = require('@adonisjs/fold');
+const { hooks } = require('@adonisjs/ignitor');
+const RouteStore = require('@adonisjs/framework/src/Route/Store');
+const Route = require('@adonisjs/framework/src/Route/index');
 
 const StaticDocs = require('../middleware/StaticDocs');
 const path = require('path');
@@ -68,7 +59,14 @@ class AdonisApiDocsProvider extends ServiceProvider {
      * @return {void}
      */
     registerDocRouter() {
-        const handler = ({ response }) => response.status(418).send('Hello World');
+        const handler = ctx => {
+            const index = fs.readFileSync(`${outPath}/index.html`, 'utf-8');
+            
+            const response = ctx.response.response
+            response.writeHeader(200, { "Content-Type": "text/html" });
+            response.write(index);
+            response.end();
+        };
 
         const routeInstance = new Route('/docs', handler, ['GET']);
         RouteStore.add(routeInstance);
