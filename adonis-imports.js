678664dda445da1b4da9fe8f03c70bf8f1f4a56a
alepontes <alexandrelopespontes@gmail.com>
2021-03-16 09:43:29 -0300

    fix: template rendering

--- adonis-imports.js
+++ adonis-imports.js
@@ -3,6 +3,12 @@ const { hooks } = require('@adonisjs/ignitor');
 const RouteStore = require('@adonisjs/framework/src/Route/Store');
 const Route = require('@adonisjs/framework/src/Route/index');
 
+// const adonis = `../../test-api/node_modules/@adonisjs`;
+// const { ServiceProvider } = require(`${adonis}/fold`);
+// const { hooks } = require(`${adonis}/ignitor`);
+// const RouteStore = require(`${adonis}/framework/src/Route/Store`);
+// const Route = require(`${adonis}/framework/src/Route/index`);
+
 /** 
  * AdonisJS Internal Imports
  */
